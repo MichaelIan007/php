655332115e7e3dfe49648a1c172fd13946583059
MichaelIan007 <iandeneckerlc@gmail.com>
2018-11-08 12:31:29 +0200

    ghost test

---
+++ Class Activities/06-10-2018/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Variables</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+</head>
+<body>
+    
+
+
+<?php
+
+$example = 'Where in the world is carmen sandiego';
+
+$example = rtrim($example, ' W');
+
+echo $example;
+
+$example1 = 'How is the Chocolate Sundae over there?';
+
+$exampl1 = ltrim($example1, ' C');
+
+echo $example1
+
+
+
+?>
+
+</body>
+</html>
